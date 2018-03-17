0f0c1429124bc5e3e43f9291d7fc5427e0a1651a
Pedro Cuadra <pjcuadra@gmail.com>
2018-03-17 17:15:50 +0100

    new: dev: First commit with draft functionality

---
+++ src/plantuml-parser.py
@@ -0,0 +1,54 @@
+# Copyright 2018 Pedro Cuadra - pjcuadra@gmail.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+from lark import Lark
+from sys import argv
+import os
+
+
+def getopts(argv):
+    opts = {}  # Empty dictionary to store key-value pairs.
+    while argv:  # While there are arguments left to parse...
+        if argv[0][0] is '-':  # Found a "-name value" pair.
+            if len(argv) > 1:
+                if argv[1][0] != '-':
+                    opts[argv[0]] = argv[1]
+                else:
+                    opts[argv[0]] = True
+            elif len(argv) == 1:
+                opts[argv[0]] = True
+
+        # Reduce the argument list by copying it starting from index 1.
+        argv = argv[1:]
+    return opts
+
+
+if __name__ == '__main__':
+    myargs = getopts(argv)
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    grammar_file_path = os.path.join(dir_path, "grammar", "main.ebnf")
+    f = open(grammar_file_path)
+
+    parser = Lark(f.read())
+
+    if '-i' in myargs:
+        f = open(myargs['-i'])
+    else:
+        exit(1)
+
+    if '-v' in myargs:
+        logging.basicConfig(level=logging.INFO)
+
+    print(parser.parse(f.read()))
